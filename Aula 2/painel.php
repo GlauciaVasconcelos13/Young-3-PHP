76a93c47ae154a5c28dc3144c370062cea917bd0
GlauciavVasconcelos13 <glauciagsmv@gmail.com>
2024-09-11 16:25:42 -0300

    Aula 2 - Painel de Login

---
+++ Aula 2/painel.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    $usuario = $_SESSION['user'];
+    $senha = $_SESSION['pass'];
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel</title>
+</head>
+<body>
+    <h1><?php echo 'Bem-vindo '. $usuario;?></h1>
+    <p><?php echo 'Sua senha é: '. $senha;?></p>
+</body>
+</html>

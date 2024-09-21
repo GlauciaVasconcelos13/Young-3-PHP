a159a1e85165a30aafbf583367f61ec27e64985c
Chronal-01 <>
2024-09-21 11:56:20 -0300

    Aula 4 - Vendas

--- Aula 4/index.php
+++ Aula 4/index.php
@@ -1,12 +1,70 @@
+<?php
+    session_start();
+
+    if(isset($_GET['ok'])) {
+        $itens = $_SESSION['Itens'];
+
+    }
+    else {
+        $itens = [];
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vendas</title>
-    <link rel = "stylesheet" href = "style.css">
+    <link rel="stylesheet" href="style.css">
+
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
+
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-family: "Righteous", cursive;
+    background-color: #cfbdbf;
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+}
+
+h1 {
+    text-align: center;
+    color: #ff4845;
+}
+.tabela {
+    background-color: #e5e5e5;
+    padding: 20px;
+    margin: 10px;
+    border-radius: 10px;
+}
+
+.titulo-tabela {
+    color: #035faf;
+    font-size: 20px;
+    margin: 10px;
+}
+
+.container-input {
+    
+}
+    </style>
+
+
+
 </head>
-<body>
+<body class = "body">
     <main class="container">
         <h1>Top Vendas</h1>
         <form action = "verificar.php" method = "post" class="container-input">
@@ -14,14 +72,29 @@
             <button type="submit">🔍</button>
         </form>
 
-        <table>
-            <tr>
+        <table class = "tabela">
+            <tr class = "titulo-tabela">
                 <th>Nome</th>
                 <th>Marca</th>
                 <th>Quantidade</th>
                 <th>Valor</th>
             </tr>
-            <!-- CODICO PHP -->
+            <?php if (!empty($itens)):?>
+                <?php foreach ($itens as $item):?>
+                    <tr class = "itens">
+                        <td><?php echo htmlspecialchars($item[0]);?></td>
+                        <td><?php echo htmlspecialchars($item[1]);?></td>
+                        <td><?php echo htmlspecialchars($item[2]);?></td>
+                        <td><?php echo htmlspecialchars($item[3]);?></td>
+
+                    </tr>
+                <?php endforeach;?>
+            <?php else: ?>
+                <tr>
+                    <td colspen = "4" style="text-align:center;">Nenhum item encontrado!</td>
+                </tr>
+            <?php endif;?>
+
         </table>
     </main>
 </body>

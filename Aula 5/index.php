638552d8dc5c23951330778e5a333fbe529daf98
HenriTwo <162809490+HenriTwo@users.noreply.github.com>
2024-09-28 10:35:00 -0300

    Aula 5 - Padaria

---
+++ Aula 5/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 5 - Padaria</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1 class="titulo">
+        Padaria dos sonhos
+    </h1>
+    <main class="container">
+        <table>
+            <tr class = "cabecalho">
+                <th>Nome</th>
+                <th>Quantidade</th>
+                <th>Ações</th>
+            </tr>
+
+        </table>
+    </main>
+</body>
+</html>

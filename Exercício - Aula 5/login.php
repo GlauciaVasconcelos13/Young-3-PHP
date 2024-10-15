af22c3e031f5c1b4bfafb7ad779e725cd69465a8
GlauciavVasconcelos13 <glauciagsmv@gmail.com>
2024-10-14 21:42:49 -0300

    Exercício

--- Exercício - Aula 5/login.php
+++ Exercício - Aula 5/login.php
@@ -1,24 +1,35 @@
-<?php
+!-- <?php
     include ('../Conexoes/conexao_login.php');
     
     $sql_codigo = 'SELECT * FROM itens';
     $resultado = $mysqli->query($sql_codigo);
     
-?>
+?> -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Padaria dos sonhos</title>
+    <link rel="stylesheet" href= "login.css">
 </head>
 <body>
     <main>
         <form action="info.php" method = "post">
-            <h1 class="titulo-login">
-                Login do Usuário - Padaria dos sonhos
-            </h1>
-            <div></div>
+            <div class="login">
+                <h1 class="titulo-login">
+                    Login do Usuário - Padaria dos sonhos
+                </h1>
+                <div>
+                    <label for="">User</label>
+                    <input type="text" name ="user">
+                </div>
+                <div>
+                    <label for="">Password</label>
+                    <input type="text" name = "pass">
+                </div>
+                <button type="submit">Login</button>
+            </div>
         </form>
     </main>
 </body>

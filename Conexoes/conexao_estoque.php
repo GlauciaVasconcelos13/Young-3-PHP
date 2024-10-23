a721ac35650a70912a7f5300435a340c3e01f192
GlauciavVasconcelos13 <glauciagsmv@gmail.com>
2024-10-23 20:44:41 -0300

    conexao

--- Conexoes/conexao_estoque.php
+++ Conexoes/conexao_estoque.php
@@ -10,14 +10,4 @@ if ($mysqli -> error){
     die("Falha ao conectar ao banco de dados: " . $mysqli->error);
 }
 
-$sql_codico = "SELECT * FROM estoque";
-
-$sql_query = $mysqli->query($sql_codico);
-
-$quantidades_linhas = $sql_query->num_rows;
-
-if ($quantidades_linhas > 1){
-    $dados = $sql_query->fetch_all();
-    var_dump ($dados);
-}
 ?>
--- Conexoes/conexao_login.php
+++ Conexoes/conexao_login.php
@@ -9,4 +9,15 @@ $mysqli = new mysqli($servidor, $banco_de_dados, $usuario, $senha);
 if ($mysqli -> error){
     die("Falha ao conectar ao banco de dados: " . $mysqli->error);
 }
+
+$sql_codico = "SELECT * FROM login_padaria";
+
+$sql_query = $mysqli->query($sql_codico);
+
+$quantidades_linhas = $sql_query->num_rows;
+
+if ($quantidades_linhas > 1){
+    $dados = $sql_query->fetch_all();
+    var_dump ($dados);
+}
 ?>
